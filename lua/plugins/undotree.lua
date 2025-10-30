a58c3a562389aa75c40b49244826ade8a2d9be0b
empeK <darkhides@gmail.com>
2025-10-30 16:28:42 +0200

    Some cleanup

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,10 +1,7 @@
 return {
 	"jiaoshijie/undotree",
 	commit = "1c3e7531fad0a9c27942c5438be5adbd26260470",
-	opts = {
-		-- your options
-	},
-	keys = { -- load the plugin only when using it's keybinding:
+	keys = {
 		{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
 	},
 }
